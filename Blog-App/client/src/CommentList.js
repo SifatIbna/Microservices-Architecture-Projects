83c4e66ac8cde32365798ea40ef7a352b37d436d
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2021-07-12 20:08:24 +0600

    returning rendered comment List

--- Blog-App/client/src/CommentList.js
+++ Blog-App/client/src/CommentList.js
@@ -1,22 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const renderedComments = comments.map(comment => {
+export default ({ comments }) => {
+  console.log(comments);
+  const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
 
