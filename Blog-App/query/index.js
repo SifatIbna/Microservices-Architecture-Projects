87ea5a598cb0d99207897224ec68ea0e9b5334d9
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-03-06 21:28:54 +0600

    updated comment upon receving commentupdated signal from event bus

--- Blog-App/query/index.js
+++ Blog-App/query/index.js
@@ -21,12 +21,21 @@ app.post("/events", (req, res) => {
   }
 
   if (type === "commentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId,status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content,status });
   }
 
+  if(type === "commentUpdated") {
+    const {id,content,postId,status} = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {return comment.id === id;});
+
+    comment.status = status;
+    comment.content = content;
+  }
   console.log(posts);
 
   res.send({});
