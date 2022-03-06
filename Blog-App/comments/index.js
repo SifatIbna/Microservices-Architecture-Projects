47dee03588133e50c4cfc775b8bb57c2194a47f5
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-03-06 21:27:15 +0600

    receving moderated comment type updated the comment status

--- Blog-App/comments/index.js
+++ Blog-App/comments/index.js
@@ -24,7 +24,7 @@ app.post("/posts/:id/comments", (req, res) => {
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content: content });
+  comments.push({ id: commentId, content: content,status:'pending' });
   commentsByPostId[req.params.id] = comments;
 
   console.log(comments);
@@ -39,6 +39,7 @@ app.post("/posts/:id/comments", (req, res) => {
         id: commentId,
         content,
         postId: req.params.id,
+        status:'pending'
       },
     },
   })
@@ -50,8 +51,29 @@ app.post("/posts/:id/comments", (req, res) => {
   res.status(201).send("OK");
 });
 
-app.post("/events", (req, res) => {
+app.post("/events",async (req, res) => {
   console.log("Received Event", req.body.type);
+
+  const {type,data} = req.body;
+  if(type === 'commentModerated') {
+    const {postId,id,status,content} = data;
+
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    await axios.post("http://localhost:4005/events",{
+      type: "commentUpdated",
+      data : {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
 
   res.send({});
 });
