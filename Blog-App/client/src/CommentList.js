3a083be7a2c4526083dbf4e565ab35e938cfc318
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-03-06 21:29:48 +0600

    changed visibility of a comment by checking its status type in the client side

--- Blog-App/client/src/CommentList.js
+++ Blog-App/client/src/CommentList.js
@@ -3,7 +3,20 @@ import React from "react";
 export default ({ comments }) => {
   console.log(comments);
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+
+    let content;
+    if(comment.status === 'approved'){
+      content = comment.content;
+    }
+
+    if(comment.status === 'pending') {
+      content = "This comment is awaiting for moderation";
+    }
+
+    if(comment.status === 'rejected') {
+      content = "This comment is Rejected"
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
