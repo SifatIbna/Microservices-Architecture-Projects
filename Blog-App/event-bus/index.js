d41ecf72bf0081a730f747c399ab76694304499b
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-03-08 21:01:25 +0600

    dockerizing services by adding docker and dockerignore files

--- Blog-App/event-bus/index.js
+++ Blog-App/event-bus/index.js
@@ -10,18 +10,26 @@ app.use(
   })
 );
 
-app.post("/events", (req, res) => {
+const events = [];
+
+app.post("/events", async(req, res) => {
   const { type } = req.body;
   console.log(type);
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events",event);
+  events.push(event);
+
+  await axios.post("http://localhost:4000/events", event);
+  await axios.post("http://localhost:4001/events", event);
+  await axios.post("http://localhost:4002/events", event);
+  await axios.post("http://localhost:4003/events",event);
 
   res.send({ status: "OK" });
 });
+
+app.get('/events',(req,res)=>{
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
